33975e2564c425a7fe15901bf2cda99ce19e5582
Patrick Alves <patrick.alves@tce.pa.gov.br>
2021-06-21 13:14:24 -0300

    Perform several customizations to my use cases

---
+++ {{cookiecutter.project_repo}}/tests/test_api.py
@@ -0,0 +1,14 @@
+from starlette.testclient import TestClient
+from pprint import pprint
+from src.api.main import api
+
+# TODO: should I use settings.py
+# TODO Use dotenv ou decouple package?
+
+client = TestClient(api)
+
+def test_status():
+    response = client.get("/status")
+    assert response.status_code == 200
+    assert response.json() == {"status": "online", "api_version": settings.API_VERSION}
+
