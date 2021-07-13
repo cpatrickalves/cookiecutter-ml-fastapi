bece60794cbcee78354a739bef81ab2482db8f93
Patrick Alves <cpatrickalves@gmail.com>
2021-07-13 17:58:36 -0300

    Add a default exception handler

--- {{cookiecutter.project_repo}}/src/app/main.py
+++ {{cookiecutter.project_repo}}/src/app/main.py
@@ -2,9 +2,8 @@ import uvicorn
 from api.routes.api import router as api_router
 from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 from core.events import create_start_app_handler
-from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
-from starlette.exceptions import HTTPException
+from fastapi import FastAPI, status
+from starlette.responses import JSONResponse
 
 
 def get_application() -> FastAPI:
@@ -17,6 +16,20 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+
+
+# === General Error Handler ===#
+@app.exception_handler(Exception)
+async def validation_exception_handler(request, err):
+    base_error_message = f"Failed to execute: {request.method}: {request.url}"
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={
+            "message": base_error_message,
+            "api_version": VERSION
+            }
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False, debug=False)
